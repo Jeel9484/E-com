0da50c90fedccaae14673a0d349ace8b046bc7ce
Jeel <jeelgodhani94@gmail.com>
2025-08-20 17:33:41 +0530

    all changes have done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // 🚫 disable `any` error
+      "@typescript-eslint/no-unused-vars": "warn", // ⚠ unused vars only warning
+    },
+  },
 ];
 
 export default eslintConfig;
